d4fb6528c4fa4f0018c66b9a0c9f2b7e48c3b53e
elmouftakir <abderahim.mouftakir@gmail.com>
2023-08-24 14:12:52 -0700

    printf

--- main.h
+++ main.h
@@ -5,11 +5,11 @@
 # include <stdio.h>
 # include <unistd.h>
 
-int		ft_putchar(int c);
-int		ft_putstr(char *s);
-int		ft_putnbr(int n);
-int		ft_putnbrnonsign(unsigned int nombre);
-int		ft_puthexa(unsigned	int nombre, char x);
-int		_printf(const char *str, ...);
+int ft_putchar(int c);
+int ft_putstr(char *s);
+int ft_putnbr(int n);
+int ft_putnbrnonsign(unsigned int nombre);
+int ft_puthexa(unsigned	int nombre, char x);
+int _printf(const char *str, ...);
 
 #endif
